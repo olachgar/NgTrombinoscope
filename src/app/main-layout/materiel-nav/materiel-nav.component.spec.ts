43ba1271eac4c8f6c7f3bacdf5d46666afb50f8b
Omar LACHGAR <omar.lachgar@bluesoft-group.com>
2019-08-07 16:40:22 +0200

    add materielNav component

---
+++ src/app/main-layout/materiel-nav/materiel-nav.component.spec.ts
@@ -0,0 +1,40 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { MaterielNavComponent } from './materiel-nav.component';
+
+describe('MaterielNavComponent', () => {
+  let component: MaterielNavComponent;
+  let fixture: ComponentFixture<MaterielNavComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MaterielNavComponent],
+      imports: [
+        NoopAnimationsModule,
+        LayoutModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MaterielNavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/main-layout/materiel-nav/materiel-nav.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, share } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-materiel-nav',
+  templateUrl: './materiel-nav.component.html',
+  styleUrls: ['./materiel-nav.component.scss']
+})
+export class MaterielNavComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      share()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
