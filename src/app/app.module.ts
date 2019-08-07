7ea61f9425de0b8cfa0e4268a66a1372c32b44e2
Omar LACHGAR <omar.lachgar@bluesoft-group.com>
2019-08-07 16:30:58 +0200

    update main component & main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,34 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MainHorizontalSearchComponent} from './main-layout/main-horizontal-search/main-horizontal-search.component';
 import {MainLayoutComponent} from './main-layout/main-layout.component';
 import {MainHorizontalMenuComponent} from './main-layout/main-horizontal-menu/main-horizontal-menu.component';
-import { MaterialModule } from './material/material.module';
+import {MaterialModule} from './material/material.module';
+import { MaterielNavComponent } from './main-layout/materiel-nav/materiel-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './main-layout/dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { NavigationComponent } from './main-layout/navigation/navigation.component';
+import { TableComponent } from './main-layout/table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
     MainHorizontalMenuComponent,
-    MainHorizontalSearchComponent
+    MainHorizontalSearchComponent,
+    MaterielNavComponent,
+    DashboardComponent,
+    NavigationComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +48,19 @@ import { MaterialModule } from './material/material.module';
     FlexLayoutModule,
     FormsModule,
     ReactiveFormsModule,
-    MaterialModule
+    MaterialModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
